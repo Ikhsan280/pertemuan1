98e8c0e4624a90d039f987e3d2c2456eab9c703f
ikhsan280 <ikhsan123ramadan@gmail.com>
2021-08-04 14:58:06 +0700

    pembelajaraan

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial=scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Kelompok 5 Blog|Home</title>
+</head>
+<body>
+		<h1>Halaman home</h1>
+
+		<script type="js/javascript.js"></script>
+</body>
+</html>
